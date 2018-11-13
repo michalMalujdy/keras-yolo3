82c83584e83d449b9a644fb6592cfb399330572a
Michał Malujdy <michal.c.malujdy@student.put.poznan.pl>
2018-11-13 23:11:48 +0100

    Global config variables

--- train.py
+++ train.py
@@ -6,30 +6,34 @@ import numpy as np
 import keras.backend as K
 from keras.layers import Input, Lambda
 from keras.models import Model
-from keras.optimizers import Adam
+from keras.optimizers import Adam, RMSProp
 from keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
 
 from yolo3.model import preprocess_true_boxes, yolo_body, tiny_yolo_body, yolo_loss
 from yolo3.utils import get_random_data
 
-def _main():
-    annotation_path = './train_data/annotations.txt'
-    log_dir = './train_data/logs/'
-    classes_path = './train_data/custom_classes.txt'
-    anchors_path = './train_data/yolo_anchors.txt'
+train_dir = './../train_data'
+
+def _main(config):
+    annotation_path = train_dir + '/annotations.txt'
+    log_dir = train_dir + '/logs'
+    classes_path = train_dir +'/custom_classes.txt'
+    anchors_path = train_dir + '/yolo_anchors.txt'
     class_names = get_classes(classes_path)
     num_classes = len(class_names)
     anchors = get_anchors(anchors_path)
 
     input_shape = (416,416) # multiple of 32, hw
 
+    optimizer = resolve_optimizer(config)
+
     is_tiny_version = len(anchors)==6 # default setting
     if is_tiny_version:
         model = create_tiny_model(input_shape, anchors, num_classes,
-            freeze_body=2, weights_path='model_data/tiny_yolo_weights.h5')
+            freeze_body=2, weights_path = train_dir + '/tiny_yolo_weights.h5')
     else:
         model = create_model(input_shape, anchors, num_classes,
-            freeze_body=2, weights_path='model_data/yolo_weights.h5') # make sure you know what you freeze
+            freeze_body=2, weights_path = train_dir + 'yolo_v3_weights.h5') # make sure you know what you freeze
 
     logging = TensorBoard(log_dir=log_dir)
     checkpoint = ModelCheckpoint(log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
@@ -49,29 +53,28 @@ def _main():
     # Train with frozen layers first, to get a stable loss.
     # Adjust num epochs to your dataset. This step is enough to obtain a not bad model.
     if True:
-        model.compile(optimizer=Adam(lr=1e-3), loss={
+        model.compile(optimizer = optimizer, loss={
             # use custom yolo_loss Lambda layer.
             'yolo_loss': lambda y_true, y_pred: y_pred})
 
         model.summary()
 
-        batch_size = 32
-        print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
-        model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
-                steps_per_epoch=max(1, num_train//batch_size),
-                validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
-                validation_steps=max(1, num_val//batch_size),
-                epochs=50,
+        print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, config.batch_size))
+        model.fit_generator(data_generator_wrapper(lines[:num_train], config.batch_size, input_shape, anchors, num_classes),
+                steps_per_epoch=max(1, num_train//config.batch_size),
+                validation_data=data_generator_wrapper(lines[num_train:], config.batch_size, input_shape, anchors, num_classes),
+                validation_steps=max(1, num_val//config.batch_size),
+                epochs=config.epochs_count,
                 initial_epoch=0,
                 callbacks=[logging, checkpoint])
         model.save_weights(log_dir + 'trained_weights_stage_1.h5')
 
     # Unfreeze and continue training, to fine-tune.
     # Train longer if the result is not good.
-    if True:
+    if False:
         for i in range(len(model.layers)):
             model.layers[i].trainable = True
-        model.compile(optimizer=Adam(lr=1e-4), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
+        model.compile(optimizer = optimizer, loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
         print('Unfreeze all of the layers.')
 
         batch_size = 32 # note that more GPU memory is required after unfreezing the body
@@ -187,5 +190,31 @@ def data_generator_wrapper(annotation_lines, batch_size, input_shape, anchors, n
     if n==0 or batch_size<=0: return None
     return data_generator(annotation_lines, batch_size, input_shape, anchors, num_classes)
 
+
+def resolve_config_globals():
+    if 'optimizer' not in globals():
+        config.optimizer = 'Adam'
+
+    if 'learning_rate' not in globals():
+        config.learning_rate = '0.0001'
+
+    if 'epochs_count' not in globals():
+        config.epochs_count = '50'
+
+    if 'batch_size' not in globals():
+        config.batch_size = '32'
+
+    return config
+
+
+def resolve_optimizer(config):
+    if config.optimizer is 'Adam':
+        return Adam(lr = config.learning_rate)
+
+    if config.optimizer is 'RMSProp':
+        return RMSProp(lr = config.learning_rate)
+
+
 if __name__ == '__main__':
-    _main()
+    config = resolve_config_globals()
+    _main(config)
