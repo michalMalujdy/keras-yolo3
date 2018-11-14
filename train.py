58c54c57e55bc0b5b50c28dda9a794612000d041
Michał Malujdy <michal.c.malujdy@student.put.poznan.pl>
2018-11-14 15:52:41 +0100

    fix

--- train.py
+++ train.py
@@ -11,7 +11,6 @@ from keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau, Ear
 
 from yolo3.model import preprocess_true_boxes, yolo_body, tiny_yolo_body, yolo_loss
 from yolo3.utils import get_random_data
-from train_config import TrainConfig
 
 train_dir = './train_data'
 
